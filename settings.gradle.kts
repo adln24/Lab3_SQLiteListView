fe7ceb78817bda20525bad1ea9beadcb3a797abc
Adlina Misha Mohamad Azli <2023437556@student.uitm.edu.my>
2023-11-30 22:14:22 +0800

    Commit SQLITE + CRUD (List View) Lab3 - Offline Database

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AdlinaMisha_Lab3OfflineDatabase"
+include(":app")
+ 
